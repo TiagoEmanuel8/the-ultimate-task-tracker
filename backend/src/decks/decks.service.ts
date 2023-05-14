1ebf3b49d3b0ade2e5046b349332cc44a31ca0ac
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-14 20:34:44 -0300

    fix: adicionando tipagem ts

--- backend/src/decks/decks.service.ts
+++ backend/src/decks/decks.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateDeckDto } from './dto/create-deck.dto';
 import { Deck } from './schema/deck.schema';
-// import { IDeck } from './interface/decks.interface';
+import { IDeck } from './interface/decks.interface';
 import { Model } from 'mongoose';
 import { UpdateDeckDto } from './dto/update-deck.dto';
 
@@ -10,12 +10,12 @@ import { UpdateDeckDto } from './dto/update-deck.dto';
 export class DecksService {
   constructor(@InjectModel(Deck.name) private deckModel: Model<Deck>) {}
 
-  async create(createDeckDto: CreateDeckDto) {
+  async create(createDeckDto: CreateDeckDto): Promise<IDeck> {
     const createdTask = new this.deckModel(createDeckDto);
     return createdTask.save();
   }
 
-  async findAll() {
+  async findAll(): Promise<IDeck[]> {
     const deckData = await this.deckModel.find();
     if (!deckData || deckData.length == 0) {
       throw new NotFoundException('Tasks data not found!');
