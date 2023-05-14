1bcaa228a4c6c33bb46dc9606448481f31f65f33
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-05-14 18:22:27 -0300

    feat: instalando mongoose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DecksModule } from './decks/decks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DecksModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
